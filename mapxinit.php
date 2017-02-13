38a640032090089626277f1a58c5c42019e906db
marfanie <margeaux.trujillo@gmail.com>
2017-02-13 07:38:47 -0500

    0001005
    sube

---
+++ mapxinit.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Untitled Document</title>
+<!-- ScreenMT -->
+<link href="/mapxtest/css/scland01mt.css" rel="stylesheet" type="text/css" media="only screen and (min-width:768px)" />
+</head>
+
+<body>
+<header>
+	<div class="wrapper">
+	<div class="logo">	
+    	Mapx
+    </div>
+    <nav>
+        <a href="/mapxtest/logout.php" target="_self" class="header-button primary-button">Cerrar Sesión</a>
+	</nav>
+    </div>
+</header>
+<div class="menu">
+
+</div>
+<div class="main">
+
+</div>
+</body>
+</html>
